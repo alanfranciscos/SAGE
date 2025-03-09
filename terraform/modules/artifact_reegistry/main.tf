1ff2a25720b97c2377276e9184dd6c0e207172c9
Alan Silva <alanfranciscos@gmail.com>
2025-03-09 19:40:06 -0300

    feature: configure terraform

---
+++ terraform/modules/artifact_reegistry/main.tf
@@ -0,0 +1,17 @@
+resource "google_artifact_registry_repository" "artifact_registry" {
+  location      = var.location
+  repository_id = var.repository_id
+  format        = "DOCKER"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.location
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform-faitec-simac"
+    prefix = "state/artifacrt_registry"
+  }
+}
