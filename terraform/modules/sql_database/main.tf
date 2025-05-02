88c4e62970305607a9b370d80c7d23fa485f1ef0
Alan Silva <alanfranciscos@gmail.com>
2025-05-02 14:56:05 -0300

    feat: fix service network

--- terraform/modules/sql_database/main.tf
+++ terraform/modules/sql_database/main.tf
@@ -1,10 +1,34 @@
+#  O Cloud SQL precisa que o Service Networking esteja habilitado corretamente na VPC.
+
 provider "google" {
   project = var.project_id
   region  = var.location
 }
 
+resource "google_project_service" "service_networking" {
+  service = "servicenetworking.googleapis.com"
+  project = var.project_id
+}
+
+
 data "google_compute_network" "default" {
   name = "default"
+}
+
+resource "google_compute_global_address" "private_ip_alloc" {
+  name          = "sql-private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = data.google_compute_network.default.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = data.google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
+
+  depends_on = [google_project_service.service_networking]
 }
 
 resource "google_sql_database_instance" "postgres-sage" {
@@ -14,8 +38,7 @@ resource "google_sql_database_instance" "postgres-sage" {
   root_password    = var.root_password
 
   settings {
-    tier = "db-f1-micro"
-
+    tier            = "db-f1-micro"
     disk_type       = "PD_HDD"
     disk_autoresize = false
     disk_size       = 10
@@ -31,11 +54,13 @@ resource "google_sql_database_instance" "postgres-sage" {
     maintenance_window {
       day          = 7
       hour         = 3
-      update_track = "canary" # 
+      update_track = "canary"
     }
 
   }
   deletion_protection = false
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
 }
 
 resource "google_sql_user" "postgres_user" {
