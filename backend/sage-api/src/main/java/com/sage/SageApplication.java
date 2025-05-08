e7f69feffb5ee94fac37ae01594770f1168036bd
Alan Silva <alanfranciscos@gmail.com>
2025-05-07 21:45:43 -0300

    refactor: improve version retrieval in Settings class

--- backend/sage-api/src/main/java/com/sage/SageApplication.java
+++ backend/sage-api/src/main/java/com/sage/SageApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SageApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SageApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SageApplication.class, args);
+    }
 
 }
