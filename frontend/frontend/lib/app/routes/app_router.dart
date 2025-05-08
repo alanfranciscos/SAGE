24e97f99f38dcc5f988baea87d4c2afe9217caba
Leandro Leite <131218893+LeandroASLeite@users.noreply.github.com>
2025-05-08 14:22:49 -0300

    Feat  imp sidebar (#6)
    * feat: sidebar implementation
    
    * feat: fix sidebar rounded and change logo img

--- frontend/frontend/lib/app/routes/app_router.dart
+++ frontend/frontend/lib/app/routes/app_router.dart
@@ -1,5 +1,6 @@
+import 'package:frontend/modules/home/settings/settings_page.dart';
 import 'package:go_router/go_router.dart';
-import '../../modules/login/login_page.dart';
+import '../../modules/login/pages/login_page.dart';
 import '../../modules/home/home_page.dart';
 
 final router = GoRouter(
@@ -7,5 +8,6 @@ final router = GoRouter(
   routes: [
     GoRoute(path: '/home', builder: (context, state) => const HomePage()),
     GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
+    GoRoute(path: '/settings', builder: (context, state) => const SettingsPage()),
   ],
 );
---
+++ frontend/frontend/lib/modules/home/settings/settings_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/modules/home/widgets/sidebar.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final bool isDesktop = MediaQuery.of(context).size.width >=800;
+    
+    return Scaffold(
+      appBar: isDesktop ? null : AppBar(title: const Text('Settings'),),
+      drawer: isDesktop ? null : const Drawer(child: Sidebar(),),
+      body: Row(
+        children: [
+          if(isDesktop) const SizedBox(width: 250, child: Sidebar(),),
+          const Expanded(child: Center(child: Text('Settings Page'))),
+        ],
+      ),
+    );
+  }
+}
