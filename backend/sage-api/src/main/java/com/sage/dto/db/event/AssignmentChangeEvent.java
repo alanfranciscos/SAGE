615693434eb364ab76dd4881dec22cb5daff25a9
Alan Silva <alanfranciscos@gmail.com>
2025-05-12 19:12:44 -0300

    feat: add AssignmentChangeEvent class for handling assignment change events

---
+++ backend/sage-api/src/main/java/com/sage/dto/db/event/AssignmentChangeEvent.java
@@ -0,0 +1,31 @@
+package com.sage.dto.db.event;
+
+import org.json.JSONObject;
+
+import com.sage.model.CaregiverAssignmentResident;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class AssignmentChangeEvent {
+
+    private String operation;
+    private String table;
+    private CaregiverAssignmentResident data;
+
+    public AssignmentChangeEvent parseJson(JSONObject jsonObject) {
+        this.operation = jsonObject.getString("operation");
+        this.table = jsonObject.getString("table");
+
+        this.data = new CaregiverAssignmentResident();
+        this.data.parseJson(jsonObject.getJSONObject("data"));
+
+        return this;
+    }
+}
