38b764e9b8f96d495459f0b67041bcb47bf55e93
Alan Silva <alanfranciscos@gmail.com>
2025-06-28 01:40:12 -0300

    feat: add searchResident endpoint to ResidentController for searching residents by ID

--- backend/sage-api-offline/src/main/java/com/sage/controller/v1/resident/ResidentController.java
+++ backend/sage-api-offline/src/main/java/com/sage/controller/v1/resident/ResidentController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * ResidentController provides endpoints for managing residents in the system.
+ * It allows for creating, updating, listing, and retrieving details of
+ * residents.
+ *
+ * @author Alan Francisco Silva
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/v1/resident")
 public class ResidentController {
@@ -29,6 +38,11 @@ public class ResidentController {
     @GetMapping("/details")
     public String getResidentDetails() {
         return "TODO: Implement resident details logic";
+    }
+
+    @GetMapping()
+    public String searchResident(@RequestParam(required = true) String search) {
+        return "TODO: Implement resident search by ID logic";
     }
 
 }
