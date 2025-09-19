d43b5162cc1184ef16dbce0d05bba80e5e9ef982
Leandro Leite <leandroleite.ll25@proton.me>
2025-09-19 13:07:59 -0300

    feat: wip alerts page

--- frontend/sage-frontend-offline/src/app/pages/alerts/alerts.component.ts
+++ frontend/sage-frontend-offline/src/app/pages/alerts/alerts.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
-import { MainComponent } from "../../layout/main/main.component";
-import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { Component, OnInit } from '@angular/core';
+import { MainComponent } from '../../layout/main/main.component';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CommonModule } from '@angular/common';
+import { ResidentService } from '../../controller/resident/resident.service';
 
 @Component({
   selector: 'app-alerts',
   standalone: true,
-  imports: [MainComponent, SearchInputComponent],
+  imports: [MainComponent, SearchInputComponent, CommonModule],
   templateUrl: './alerts.component.html',
-  styleUrl: './alerts.component.scss'
+  styleUrl: './alerts.component.scss',
 })
-export class AlertsComponent {
-
-  constructor(){}
-
+export class AlertsComponent implements OnInit {
+  constructor(private residentService: ResidentService) {}
+  async ngOnInit(): Promise<void> {
+    this.totalActiveCalls =
+      await this.residentService.getTotalActiveResidentsCalls();
+  }
+  totalActiveCalls: number = 0;
+  private iconMap: Record<string, string> = {
+    dashboard: 'fa-solid fa-chart-line',
+    alerts: 'fa-solid fa-bell',
+    reports: 'fa-solid fa-file-lines',
+    settings: 'fa-solid fa-gear',
+  };
   async onSearch(searchTerm: string) {
     console.log('Search term:', searchTerm);
   }
 
+  getIconClass(item: string): string {
+    return this.iconMap[item] || 'fa-solid fa-circle-question';
+  }
 }
