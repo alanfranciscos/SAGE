115c9d8f904e6a24c712a68070866ffec4b6e580
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2025-09-30 21:37:58 -0300

    feat: implementacao do logout

--- frontend/sage-frontend-offline/src/app/pages/nurse-manager/nurse-manager.component.ts
+++ frontend/sage-frontend-offline/src/app/pages/nurse-manager/nurse-manager.component.ts
@@ -4,18 +4,51 @@ import { ListNurseComponent } from '../../components/list-nurse-component/list-n
 import { InstructionsComponent } from '../../components/instructions/instructions.component';
 import { MainComponent } from '../../layout/main/main.component';
 import { RegisterCaregiverLeaderComponent } from "../../components/register-caregiver-leader/register-caregiver-leader.component";
+import { NgIf } from '@angular/common';
+import { AuthenticationService } from '../../services/authentication/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nurse-manager',
   standalone: true,
   imports: [
+    NgIf,
     RegisterNurseComponentComponent,
     ListNurseComponent,
     InstructionsComponent,
     MainComponent,
     RegisterCaregiverLeaderComponent
-],
+  ],
   templateUrl: './nurse-manager.component.html',
   styleUrl: './nurse-manager.component.scss',
 })
-export class NurseManagerComponent {}
+export class NurseManagerComponent {
+  isUserMenuOpen = false;
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) { }
+
+  toggleUserMenu() {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  closeUserMenu() {
+    setTimeout(() => {
+      this.isUserMenuOpen = false;
+    }, 150);
+  }
+
+  onEditProfile(event: Event) {
+    event.stopPropagation();
+    console.log('Editar perfil clicado');
+  }
+
+  onLogout(event: Event) {
+    event.stopPropagation();
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+}
+
