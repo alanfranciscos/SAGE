221654c48a3eebef462be1d2c6a4e952a2ec3e70
Leandro Leite <leandroleite.ll25@proton.me>
2025-10-02 15:52:49 -0300

    feat:sidebar-button

--- frontend/sage-frontend-offline/src/app/layout/main/sidebar/sidebar.component.ts
+++ frontend/sage-frontend-offline/src/app/layout/main/sidebar/sidebar.component.ts
@@ -5,11 +5,18 @@ import { CommonModule, NgClass } from '@angular/common';
 import { TooltipWrapperComponent } from '../../../components/tooltip-wrapper/tooltip-wrapper.component';
 import { ResidentService } from '../../../controller/resident/resident.service';
 import { SseService } from '../../../controller/sse/sse.service';
+import { SettingsService } from '../../../controller/settings/settings.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, TooltipWrapperComponent, NgClass, CommonModule],
+  imports: [
+    RouterLink,
+    TooltipWrapperComponent,
+    NgClass,
+    CommonModule,
+    ButtonComponent,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
@@ -20,11 +27,12 @@ export class SidebarComponent implements OnInit {
     ['settings', '/settings'],
     ['profile', '/profile'],
   ]);
-
+  alarmStatusOnline: boolean = true;
   constructor(
     private router: Router,
     private residentService: ResidentService,
-    private sseService: SseService
+    private sseService: SseService,
+    private settingsService: SettingsService
   ) {}
   totalActiveCalls: number = 0;
 
@@ -44,16 +52,17 @@ export class SidebarComponent implements OnInit {
     this.activeItem = this.router.url;
     this.totalActiveCalls =
       await this.residentService.getTotalActiveResidentsCalls();
-    this.residentService.totalActiveCalls$.subscribe((total) => {
+    this.residentService.totalActiveCalls$.subscribe(async (total) => {
       this.totalActiveCalls = total;
+
+      await this.loadAlarmStatus();
     });
 
     this.sseService.messages$.subscribe(async (msg) => {
       if (!msg) return;
 
-
       this.totalActiveCalls =
-      await this.residentService.getTotalActiveResidentsCalls();
+        await this.residentService.getTotalActiveResidentsCalls();
       this.residentService.totalActiveCalls$.subscribe((total) => {
         this.totalActiveCalls = total;
       });
@@ -66,5 +75,18 @@ export class SidebarComponent implements OnInit {
 
   getActiveRoute(): string {
     return this.router.url;
+  }
+  async loadAlarmStatus() {
+    try {
+      const settings =
+        await this.settingsService.getPanelSettingsBySerialNumber('2785040674');
+      this.alarmStatusOnline = settings.status === 'Online';
+    } catch (error) {
+      console.error('Erro ao buscar status da central', error);
+      this.alarmStatusOnline = false; // assume offline em erro
+    }
+  }
+  goToAlarmPanel() {
+    this.router.navigate(['/settings']); // ou rota que você quiser
   }
 }
